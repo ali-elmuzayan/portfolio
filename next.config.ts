eb2a25a9cf7a3f120d39eaffd0bb82c7bf8f7816
ali-elmuzayan <ali.elmuzayn@gmail.com>
2026-09-16 14:33:25 +0300

    starting from the zero to implement a new nextJS and implement a new design

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  logging: {
-    fetches: { fullUrl: true, hmrRefreshes: true },
-  },
 };
 
 export default nextConfig;
